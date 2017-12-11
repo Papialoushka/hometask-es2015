9c0d88286a824901c828cd7c6c625791844d18d9
Alena K <Alena_Korachkina@epam.com>
2017-12-11 18:21:21 +0300

    New directories, js and css

---
+++ src/scripts/render-resources.js
@@ -0,0 +1,25 @@
+import NewsResource from './news-resources-link';
+import './../styles/screen.css'
+import './../styles/resources.css';
+
+export default function renderResources() {
+  const apiKey = '59c232ebefc84b5d9c489f4222161cf2',
+        apiUrl = `https://newsapi.org/v2/sources?apiKey=${apiKey}`,
+        getResponse = (response) => response.json(),
+        getResources = (resources) => {
+          resources.sources.forEach(source => {
+            const newResourceItem = new NewsResource(source);
+
+            newResourceItem.createLink();
+          });
+          return resources.sources.id;
+        };
+
+  const request = async() => {
+    const response = await fetch(apiUrl);
+    const json = await getResponse(response);
+    const parseJson = await getResources(json);
+  };
+
+  request();
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const webpack = require('webpack');
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 module.exports = {
   entry: [
-    //'babel-polyfill',
-    './src/news-module.js'
+    'babel-polyfill',
+    './src/scripts/news-module.js'
+  ],
+  watch: NODE_ENV === 'development',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(NODE_ENV)
+    })
   ],
   output: {
     filename: 'news-module.js',
@@ -14,17 +23,33 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader?cacheDirectory=true",
+        loader: 'babel-loader?cacheDirectory=true',
         include: [
-          path.resolve(__dirname, "src"),
+          path.resolve(__dirname, 'src'),
         ]
       },
       {
         test: /\.css$/,
         use: [
-          'style-loader',
-          'css-loader'
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
         ]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader'
+          }
+        ]
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader!./loaders/remove-number-attribute-loader'
       }
     ]
   }
